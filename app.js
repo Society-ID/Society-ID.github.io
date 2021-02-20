625493c278e5147c8859f69467c749fbd5f60503
Arief Purnama Muharram <ariefpurnamamuharram@gmail.com>
2021-02-20 21:20:42 +0700

    Add menu overlay on 2021/02/20.

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ $(document).ready(function () {
         $(this).toggleClass('open');
     });
 
+    $('.menu-item').click(function () {
+        $('#cbToggler').prop('checked', false)
+    })
+
     $('#fullpage').fullpage({
+        anchors: ['anchor0', 'anchor1', 'anchor2', 'anchor3', 'anchor4', 'anchor5'],
         navigation: true,
         navigationPosition: 'left',
     })
