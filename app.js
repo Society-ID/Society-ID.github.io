78f5adcd4550555606b1e086bea18a8da3b438f3
Arief Purnama Muharram <ariefpurnamamuharram@gmail.com>
2021-02-20 16:38:41 +0700

    Update on 2021/02/20.

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+    $('.arrow-button, .arrow-button-sm').click(function () {
+        $(this).toggleClass('open');
+    });
+
     $('#fullpage').fullpage({
         navigation: true,
         navigationPosition: 'left',
